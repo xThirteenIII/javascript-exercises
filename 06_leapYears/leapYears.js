f98acaa458a0fecd34df18ba86a6420fc95f7ee5
th1rt3en <giuseppe.richetti@gmail.com>
2023-10-04 17:43:46 +0200

    Add leapYears solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+  if (isCentury(year)){
+    return year%400==0
+  }else {
+    return year%4==0
+  }
 };
+
+function isCentury(year){
+
+  return year%100 == 0
+}
 
 // Do not edit below this line
 module.exports = leapYears;
