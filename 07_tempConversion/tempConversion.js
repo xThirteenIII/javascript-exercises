2253b29b029c5b899b83a27c5f8f373fbaf4df14
th1rt3en <giuseppe.richetti@gmail.com>
2023-10-04 17:50:54 +0200

    Add solution to convertToCelsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,7 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+
+  // 10/10 rounds to 1 decimal, 100/100 to 2, and so on
+  return Math.round((fahrenheit - 32) * (5/9) * 10)/10
 };
 
 const convertToFahrenheit = function() {
