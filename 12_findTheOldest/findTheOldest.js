3f07a0358dbfd32f262acb26b526cf98a3ce6bf7
Jisaa <giuseppe.richetti@gmail.com>
2023-10-26 15:47:05 +0200

    Add section 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const CURRENT_YEAR = 2023
 
+const findTheOldest = function(arrayOfPeople) {
+
+  let name = "";
+  let oldest = 0;
+
+
+  arrayOfPeople.forEach(person=>{
+
+
+    if ("yearOfDeath" in person){
+      if (person.yearOfDeath - person.yearOfBirth > oldest) {
+        name = person.name;
+        oldest = person.yearOfDeath - person.yearOfBirth;
+      }
+    }else{
+      if (CURRENT_YEAR - person.yearOfBirth > oldest){
+
+        name = person.name;
+        oldest = CURRENT_YEAR - person.yearOfBirth;
+      }
+    }
+
+  
+  })
+
+  return name
 };
 
 // Do not edit below this line
